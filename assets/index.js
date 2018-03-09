26b01bee3b9ca90de2e8921f81a68a10c7694411
Clifford Crerar <cliff.crerar@gmail.com>
2018-03-09 23:22:40 +0200

    setup webpack

---
+++ assets/index.js
@@ -0,0 +1,20 @@
+/* --------------------------- */
+/*          MAIN ENTRY         */
+/* --------------------------- */
+
+/*<link rel="stylesheet" type="text/css" href="assets/fonts/fontawesome/font-awesome.min.css">*/
+import './fonts/fontawesome/font-awesome.min.css';
+/*<link rel="stylesheet" type="text/css" href="assets/fonts/pe-icon/pe-icon.css">*/
+import './fonts/pe-icon/pe-icon.css';
+/*<!-- Vendors-->*/
+/*<link rel="stylesheet" type="text/css" href="assets/vendors/bootstrap/grid.css">*/
+import './vendors/bootstrap/grid.css';
+/*<link rel="stylesheet" type="text/css" href="assets/vendors/magnific-popup/magnific-popup.min.css">*/
+import './vendors/magnific-popup/magnific-popup.min.css';
+/*<link rel="stylesheet" type="text/css" href="assets/vendors/swiper/swiper.css">*/
+import './vendors/swiper/swiper.css';
+/*<!-- App & fonts-->*/
+/*<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700|Permanent+Marker|Cinzel|Righteous">*/
+import 'https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700|Permanent+Marker|Cinzel|Righteous';
+/*<link rel="stylesheet" type="text/css" href="assets/css/main.css">*/
+import './css/main.css';
