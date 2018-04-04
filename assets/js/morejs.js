26859726dffed62f5bbe2db63adea29cda7e4666
CliffordCrerar <cliff.crerar@gmail.com>
2018-04-04 11:04:41 +0200

    Loading screen

--- assets/js/morejs.js
+++ assets/js/morejs.js
@@ -1,19 +1,33 @@
-$('.onepage-menu').on('click', function () {
-  if (screen.width <= 414) {
-    $(this).hide();
-  }
+// Loading options
+var loadscreenTimeout = 3000;
+var slideFade = 'Slide';
+// Loading options
+$(window).on('load', function() {
+    if (slideFade == 'Fade') {
+        setTimeout(function() { $('#loader').fadeOut(); }, loadscreenTimeout);
+    } else if (slideFade == 'Slide') {
+        setTimeout(function() { $('#loader').slideUp(); }, loadscreenTimeout);
+    }
+
+    setTimeout(function() { $('#loader').fadeOut(); }, loadscreenTimeout);
 });
 
-$('.navbar-toggle').on('click', function () {
-  if (screen.width <= 414) {
-    $('.onepage-menu').show();
-  }
+$('.onepage-menu').on('click', function() {
+    if (screen.width <= 414) {
+        $(this).hide();
+    }
+});
+
+$('.navbar-toggle').on('click', function() {
+    if (screen.width <= 414) {
+        $('.onepage-menu').show();
+    }
 });
 
 if (screen.width <= 414) {
-  $('.hero__title').css('color', '#660000');
-  $('.hero__title').css('width', screen.width);
-  $('.typed').css('width', screen.width);
-  $('.hero__text').css('width', screen.width);
-  $('.hero__title').css('height', screen.height / 4);
+    $('.hero__title').css('color', '#660000');
+    $('.hero__title').css('width', screen.width);
+    $('.typed').css('width', screen.width);
+    $('.hero__text').css('width', screen.width);
+    $('.hero__title').css('height', screen.height / 4);
 }
